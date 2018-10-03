1d45998b34b7748d99386a4084148401cccd96ab
Piotrek <piotrek.milczarek@gmail.com>
2018-10-03 10:39:04 +0200

    Init commit with project structure

---
+++ WordCount/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
